abc2998114a65fc09ec195c29a2b410cedb2ffae
tokorom <tokorom@gmail.com>
2012-09-06 17:38:51 +0900

    Fix problem 4

--- 4.hs
+++ 4.hs
@@ -1,16 +1,6 @@
 -- Project Euler Problem 4
 
-isPalindromic n = isPalindromicForString $ show n
-isPalindromicForString s
-  | (1 >= (length s)) = True
-  | ((head s) == (last s)) = isPalindromicForString $ tail $ init s
-  | otherwise = False
+isPalindromic n = (show n) == (reverse $ show n)
+searchPalindromic n m = maximum [x*y | x <- [n..m], y <-[x..m], isPalindromic $ x*y]
 
-searchPalindromic max = searchPalindromicForProduct max max max
-searchPalindromicForProduct a b max
-  | (100 > a) = (0, 0, 0)
-  | isPalindromic (a * b) = (a, b, a * b)
-  | (a == b) = searchPalindromicForProduct (a - 1) max max
-  | otherwise = searchPalindromicForProduct a (b - 1) max
-
-main = print $ searchPalindromic 999 -- (924, 962, 888888)
+main = print $ searchPalindromic 100 999 -- 906609
